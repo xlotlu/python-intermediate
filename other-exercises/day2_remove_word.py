38c8f5b021b2c13d0efb711639f44a55904fb4dc
Ionuț Ciocîrlan <ionut.ciocirlan@gmail.com>
2025-07-23 10:26:09 +0300

    the remove word exercise

--- other-exercises/day2_remove_word.py
+++ other-exercises/day2_remove_word.py
@@ -1,31 +1,68 @@
-# filter_by_agewrite a function that returns an input string, with a given parameter removed
+# write a function that processes an input string
+# and returns it with a specific word removed
 
 def remove_word(input_txt, word):
+    """
+    removes all occurences of `word` from `input_txt`
+    """
+
+    # we instantiate the output as an empty string.
+    # we will add to this every character that does not match
+    # the first character from `word`
+
     output = ""
+
+    # we transform `word` into an iterator, so that we can seek
+    # on its first character, and when found advance the cursor
+    # to the next.
+    # (this will help us by not needing to remember
+    # the cursor position.)
 
     chrs = iter(word)
     seeking = next(chrs)
 
+    # this will be a buffer to preserve partially matched strings,
+    # in order to glue them back in case of a partial match.
+    #
+    # it will also help us know if we are currently in the process of matching.
+    current_match = ""
+
+    # we iterate through the input character by character,
+    # looking for the current match
     for chr in input_txt:
         if chr == seeking:
+            current_match += chr
+
+            # if we found the current character in `word`,
+            # try to advance the cursor to the next
             try:
                 seeking = next(chrs)
             except StopIteration:
+                # this means we found all characters in `word`.
+                # this is now a full match, so we reset everything
                 chrs = iter(word)
                 seeking = next(chrs)
-
-            # we_are_in_the_middle_of_matching
-            continue
+                current_match = ""
 
         else:
-            #if we_are_in_the_middle_of_matching:
-            #    1) vezi... că ai păpat niște caractere
-            #    2) vezi că trebuie să resetezi pointerul
+            # this is not a match.
+
+            if current_match:
+                # if we have a partial previous match,
+                # we need to glue back the lost characters
+
+                output += current_match
+
+                # and reset everything
+                chrs = iter(word)
+                seeking = next(chrs)
+                current_match = ""
+
             output += chr
 
     return output
 
-print('[result]', remove_word(
-    'this is a wordpress full of words with swords',
-    'word'
-))
+#print('[result]', remove_word(
+#    'this is a wordpress full of words with swords',
+#    'word'
+#)
