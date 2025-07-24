9eb7877126c6d1c9d781b12bc50942f4a4f854e8
Ionuț Ciocîrlan <ionut.ciocirlan@gmail.com>
2025-07-24 17:51:04 +0300

    the specs

--- other-exercises/day4_tuples_to_csv.py
+++ other-exercises/day4_tuples_to_csv.py
@@ -1,2 +1,25 @@
-# given the dataset
-            else:sssssssssssssssssssssssssssssssssssssss
+# 1. given the dataset
+dataset = [
+    ('Andrei Georgescu', 20, True),
+    ('George Andreescu', 28, False),
+    ('Vasi Vasilescu', 41, False),
+    ('Elena Enescu', 32, True),
+    ('Gigel Gigescu', 56, True),
+]
+
+# write it into a csv file
+
+csv.writer
+
+
+# 2. given the dataset
+dataset = [
+    {'name': 'Andrei Georgescu', 'age': 20, 'is_customer': True},
+    {'name': 'George Andreescu', 'age': 28, 'is_customer': False},
+    {'name': 'Vasi Vasilescu', 'age': 41, 'is_customer': False},
+    {'name': 'Elena Enescu', 'age': 32, 'is_customer': True},
+    {'name': 'Gigel Gigescu', 'age': 56, 'is_customer': True},
+]
+# write it into a csv file
+
+csv.DictWriter
