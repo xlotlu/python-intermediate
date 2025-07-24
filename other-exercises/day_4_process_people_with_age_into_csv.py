e9a6b765e70f36d67b4527d8e88483ebef67d7d4
Ionuț Ciocîrlan <ionut.ciocirlan@gmail.com>
2025-07-24 13:39:25 +0300

    reading people into csv updated

--- other-exercises/day_4_process_people_with_age_into_csv.py
+++ other-exercises/day_4_process_people_with_age_into_csv.py
@@ -19,8 +19,36 @@
 # - the string "max"
 # - the maximum age
 
+import csv
+import math
 from day_4_process_people_with_age import process_people 
 
 def process_people_to_csv(fname_in, fname_out):
-    rows = process_people(fname_in)
+    people = process_people(fname_in)
 
+    count = 0
+    age_min = math.inf
+    age_max = 0
+    age_sum = 0
+
+    with open(fname_out, 'w') as f:
+        writer = csv.DictWriter(f, ('name', 'age'))
+        writer.writeheader()
+
+        # write each row
+        for d in people:
+            writer.writerow(d)
+
+            count += 1
+            age_max = max(age_max, d['age'])
+            age_min = min(age_min, d['age'])
+            age_sum += d['age']
+
+        # write footer
+        footwriter = csv.writer(f)
+        footwriter.writerow([
+            "rowcount", count,
+            "average", age_sum / count,
+            "min", age_min,
+            "max", age_max,
+        ])
